06b49dee5ce328a55d8832ee1302d0f80eb2acce
dalred <RcrF63gGG7HpbAT>
2021-11-23 16:52:53 +0300

    Second step

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,15 @@
 import json, os
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
+
 def read_json(name):
-  data = {}
-  with open(name, "r", encoding = 'utf-8') as file:
+    data = {}
+    with open(name, "r", encoding='utf-8') as file:
         data = json.load(file)
-  return data
+    return data
+
 
 @app.route('/')
 def index():
@@ -16,18 +17,21 @@ def index():
         return 'Приложение работает'
     else:
         return 'Приложение не работает'
+
+
 @app.route('/candidate/<int:x>/')
 def candidat(x):
     data = read_json("candidates.json")
     id_lst = [i['id'] for i in data]
     if x not in id_lst:
-        return ('Кандидат не найден!')
+        return ('<html lang="ru"><p><b>Кандидат не найден!</b></p>')
     else:
-        data=data[x - 1]
+        data = data[x - 1]
         return f"<h1>{data['name']}</h1>" \
                f"<p>{data['position']}</p>" \
-               f'<img src="{data["picture"]}" width=200/>' \
-                                     f'<p>{data["skills"]}</p>'
+               f"<img src='{data['picture']}' width=200/>" \
+               f"<p>{data['skills']}</p>"
+
 
 if __name__ == "__main__":
-    app.run('127.0.0.1',8000)
+    app.run('127.0.0.1', 8000)
